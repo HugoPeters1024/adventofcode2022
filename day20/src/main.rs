829b5a52f0623e8e1b72f2917f478c4ba3606400
HugoPeters1024 <hpeters1024@gmail.com>
2022-12-20 14:48:14 +0100

    day 20.2

--- day20/src/main.rs
+++ day20/src/main.rs
@@ -1,4 +1,4 @@
-use std::{io::BufRead};
+use std::io::BufRead;
 
 #[derive(Debug, Clone)]
 struct Node {
@@ -13,28 +13,25 @@ fn main() {
 
     let mut nodes = Vec::new();
     for (i,number) in numbers.iter().enumerate() {
-        nodes.push(Node { value: *number, moved: false, left: (i+numbers.len()-1)%numbers.len(), right: (i+1)%numbers.len() });
+        nodes.push(Node { value: number.clone() * 811589153, moved: false, left: (i+numbers.len()-1)%numbers.len(), right: (i+1)%numbers.len() });
     }
 
+    // keep track of the nodes we visit in the first round to reuse in the later rounds
+    let mut cursors = Vec::new();
     let mut cursor = 0;
-    let mut count = 0;
-    while count < numbers.len() {
+    while cursors.len() < numbers.len() {
         let me = nodes[cursor].clone();
-
-        if me.value == 0 {
-            nodes[cursor].moved = true;
-            count += 1;
-            cursor = me.right;
-            continue;
-        }
 
         if me.moved {
             cursor = me.right;
             continue;
         }
 
-        let mut to_move = me.value;
-        let mut move_cursor = if to_move > 0 { me.right } else { to_move += 1; me.left };
+        cursors.push(cursor);
+
+        let mut to_move = me.value.rem_euclid((numbers.len() -1) as i64);
+
+        let mut move_cursor = me.right;
 
         // unlink the current node
         nodes[me.left].right = me.right;
@@ -64,7 +61,43 @@ fn main() {
         println!("{}: moves between {} and {}", me.value, nodes[nodes[cursor].left].value, nodes[nodes[cursor].right].value);
 
         cursor = me.right;
-        count += 1;
+    }
+
+    // 9 more rounds
+    for _ in 0..9 {
+        for cursor in cursors.iter().cloned() {
+            let me = nodes[cursor].clone();
+
+            let mut to_move = me.value.rem_euclid((numbers.len() -1) as i64);
+
+            let mut move_cursor = me.right;
+
+            // unlink the current node
+            nodes[me.left].right = me.right;
+            nodes[me.right].left = me.left;
+
+            while to_move > 0 {
+                move_cursor = nodes[move_cursor].right;
+                to_move -= 1;
+            }
+
+            while to_move < 0 {
+                move_cursor = nodes[move_cursor].left;
+                to_move += 1;
+            }
+
+            // I point right to the move cursor
+            nodes[cursor].right = move_cursor;
+            // I point left to where the move cursor pointed
+            nodes[cursor].left = nodes[move_cursor].left;
+            // The move cursor points left to me
+            nodes[move_cursor].left = cursor;
+            // The node to my left points right to me
+            let left_idx = nodes[cursor].left;
+            nodes[left_idx].right = cursor;
+
+            println!("{}: moves between {} and {}", me.value, nodes[nodes[cursor].left].value, nodes[nodes[cursor].right].value);
+        }
     }
 
     // find zero
@@ -97,16 +130,3 @@ fn main() {
     }
     dbg!(total);
 }
-
-fn to_vec(nodes: &Vec<Node>) -> Vec<i64> {
-    let mut cursor = 0;
-    let mut count = 0;
-    let mut result = Vec::new();
-    while count < nodes.len() {
-        result.push(nodes[cursor].value);
-        cursor = nodes[cursor].right;
-        count += 1;
-    }
-    result
-}
-
