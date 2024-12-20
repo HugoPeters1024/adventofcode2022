f1843e1ffcb49654c5acafce379bcdcae30ba84e
Hugo Peters <hugo.peters@channable.com>
2024-12-20 12:45:57 +0100

    day20 part 1

---
+++ 2024/day20/src/main.rs
@@ -0,0 +1,94 @@
+use std::{
+    collections::{HashMap, HashSet, VecDeque},
+    io::BufRead,
+};
+
+fn main() {
+    let grid: Vec<Vec<char>> = std::io::stdin()
+        .lock()
+        .lines()
+        .map(|l| l.unwrap().chars().collect())
+        .collect();
+
+    let mut start_x = 0isize;
+    let mut start_y = 0isize;
+
+    for (y, line) in grid.iter().enumerate() {
+        for (x, c) in line.iter().enumerate() {
+            if *c == 'S' {
+                start_x = x as isize;
+                start_y = y as isize;
+            }
+        }
+    }
+
+    let mut dist_from_start: HashMap<(isize, isize), isize> = HashMap::new();
+
+    let mut work: VecDeque<(isize, isize, isize)> = VecDeque::new();
+    work.push_front((start_x, start_y, 0));
+    while let Some((x, y, dist)) = work.pop_front() {
+        if grid[y as usize][x as usize] == '#' {
+            continue;
+        }
+
+        if dist_from_start.contains_key(&(x, y)) {
+            continue;
+        }
+
+        dist_from_start.insert((x, y), dist);
+
+        work.push_back((x + 1, y, dist + 1));
+        work.push_back((x - 1, y, dist + 1));
+        work.push_back((x, y + 1, dist + 1));
+        work.push_back((x, y - 1, dist + 1));
+    }
+
+    let cheat_threshold = 20;
+    let max_cheat_length = 2;
+
+    let mut cheats_found = 0;
+    let mut work: VecDeque<(isize, isize, isize)> = VecDeque::new();
+    let mut visisted: HashSet<(isize, isize)> = HashSet::new();
+    work.push_front((start_x, start_y, 0));
+    while let Some((x, y, dist)) = work.pop_front() {
+        if grid[y as usize][x as usize] == '#' {
+            continue;
+        }
+
+        if visisted.contains(&(x, y)) {
+            continue;
+        }
+        visisted.insert((x, y));
+
+        //for dy in -max_cheat_length..=max_cheat_length {
+        //    dbg!(dy);
+        //}
+
+        for dir in 0..4 {
+            let (nx,ny) = match dir {
+                0 => (x+2,y),
+                1 => (x-2,y),
+                2 => (x,y+2),
+                3 => (x,y-2),
+                _ => panic!(),
+            };
+            if let Some(potential_further) = dist_from_start.get(&(nx, ny)) {
+                if *potential_further > dist + 2 {
+                    let gain = potential_further - dist - 2;
+                    if gain >= cheat_threshold {
+                        cheats_found += 1
+                    }
+                }
+            }
+        }
+
+        dist_from_start.insert((x, y), dist);
+
+        work.push_back((x + 1, y, dist + 1));
+        work.push_back((x - 1, y, dist + 1));
+        work.push_back((x, y + 1, dist + 1));
+        work.push_back((x, y - 1, dist + 1));
+    }
+
+    println!("cheats found with threshold {}: {}", cheat_threshold, cheats_found);
+}
